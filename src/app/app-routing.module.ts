517fbda2d97f2ba6cf51ad645a6df06c37bf7573
Lisa Lewis <mrs.lisalewis@outlook.com>
2023-02-07 22:03:21 +0800

    Updated-02/07/23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CoursesComponent } from './courses/courses.component';
+import { StudentsComponent } from './students/students.component';
+import { EnrollmentComponent } from './enrollment/enrollment.component';
+import { ClassesComponent } from './classes/classes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'course/:courseId', component:CourseDetailsComponent},
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'courses', component:CoursesComponent},
+  {path: 'students', component: StudentsComponent},
+  {path: 'enrollment', component: EnrollmentComponent},
+  {path: 'classes', component: ClassesComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
